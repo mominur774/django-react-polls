73c82eda9c1f8584c32d0d5d7836172ed2a33308
Mominur Rahman <mominurrohman774@gmail.com>
2023-05-18 22:16:54 +0600

    add drf_yasg for api documentation

--- backend-polls/polls/urls.py
+++ backend-polls/polls/urls.py
@@ -4,6 +4,9 @@ from rest_framework.permissions import AllowAny
 from django.conf import settings
 from django.conf.urls.static import static
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,3 +15,16 @@ urlpatterns = [
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Polls API",
+      default_version='v1',
+      description="Polls API description",
+   ),
+   public=True,
+   permission_classes=(AllowAny,),
+)
+
+urlpatterns += path('api-docs/', schema_view.with_ui('swagger', cache_timeout=0), name='api_playground'),
